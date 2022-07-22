69a49e3ed2eab03ff80e9a4353baa43fb113f776
xuliang <xulaing@local>
2022-07-22 18:29:31 +0800

    cmake hello and muti files test

---
+++ hello/helloworld.cpp
@@ -0,0 +1,12 @@
+/**
+ * 初识cmake
+ * 
+ * */
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "hello world" << endl;
+    return 0;
+}
---
+++ main/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * 多级目录下的cmake编写,多个源文件
+ * */
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "这是根目录中的主程序文件" << endl;
+    return 0;
+}
